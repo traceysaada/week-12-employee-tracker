b5df86b39e4bfe63fac6c166fe041ecf15a3e44b
Tracey Saada <tracey.saada@outlook.com>
2021-01-25 18:59:04 +0000

    started all files & pseudo

---
+++ db/employees.js
@@ -0,0 +1,21 @@
+//first thing is to set up connection to database
+
+
+
+//write functions that will interact with data, one for every command use insert
+//add departments
+//add roles
+//add employees
+
+
+//use select * to get everything.
+//view departments
+//view roles
+//view employees 
+
+
+//uses update
+//update employee roles
+
+module.exports 
+
---
+++ index.js
@@ -0,0 +1,7 @@
+//all inquire to go in here
+
+//node index run in here
+
+//question prompts
+
+//answer selections to move to relevant next stage ie roles or department etc
