85ea0a3e3fb0f26846f34351c0d885bf815c8201
Tracey Saada <tracey.saada@outlook.com>
2021-01-30 20:26:48 +0000

    continued adding funtions for new data adding

--- db/employeeDatabase.js
+++ db/employeeDatabase.js
@@ -1,5 +1,6 @@
 const connection = require("./connection")
-
+//write functions that will interact with data, one for every command use insert
+//add departments //add roles //add employees
 class DatabaseQueries {
     constructor(connection){
         this.connection = connection
@@ -7,21 +8,36 @@ class DatabaseQueries {
     getAllDepartments(){
         return this.connection.query("SELECT * FROM department"); 
     }
+    getAllRoles(){
+        return this.connection.query("SELECT * FROM role");
+    }
+    getAllEmployees(){
+        return this.connection.query("SELECT * FROM employee");
+    }
+    addNewDepartment(newDepartment){
+        return this.connection.query("INSERT INTO department SET ?", newDepartment);
+    }
+    addNewEmployee(NewEmployee){
+        return this.connection.query("INSERT INTO employee SET ?", NewEmployee)
+    }
+//use select * to get everything.//view departments //view roles //view employees 
+
 }
 
+//uses update //update employee roles
+
 module.exports = new DatabaseQueries(connection); 
-//write functions that will interact with data, one for every command use insert
-//add departments
-
-//add roles
-//add employees
-
-//use select * to get everything.
-//view departments
-//view roles
-//view employees 
 
 
 
-//uses update
-//update employee roles
+
+
+
+
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,129 +1,132 @@
 //all inquire to go in here
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
-const databaseQueries = require("./db/employeeDatabase")
+const databaseQueries = require("./db/employeeDatabase");
 
-
-function initialise(){
-  console.log("...Loading...")
+function initialise() {
+  console.log("...Loading...");
   runSearch();
 }
 
 //question prompts
 async function runSearch() {
- const { options } = await inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "options",
-        message: "What would you like to do?",
-        choices: [
-          "View Departments",
-          "View Roles",
-          "View Employees",
-          "view Employees by Manager",
-          "Add Employee",
-          "remove Employee",
-          "Update Employee Role",
-          "Add Department",
-          "Add Role",
-          "Exit",
-        ],
-      },
-    ])
-   console.log(options)
-      switch (options) {
-        case "View Departments":
-          viewDepartments();
-          console.log("departments")
-          break;
+  const { options } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "options",
+      message: "What would you like to do?",
+      choices: [
+        "View Departments",
+        "View Roles",
+        "View Employees",
+        "View Employees by Manager",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee Role",
+        "Add Department",
+        "Add Role",
+        "Exit",
+      ],
+    },
+  ]);
+  console.log(options);
+  switch (options) {
+    case "View Departments":
+      viewDepartments();
+      break;
 
-        case "View Roles":
-          viewRoles();
-          break;
+    case "View Roles":
+      viewRoles();
+      break;
 
-        case "view Employees.":
-          viewEmployees();
-          break;
+    case "View Employees":
+      viewEmployees();
+      break;
 
-        case "view Employees by Manager":
-          viewEmployeesByManager();
-          break;
+    case "View Employees by Manager":
+      viewEmployeesByManager();
+      break;
 
-        case "add Employees":
-          addEmployee();
-          break;
+    case "Add Employees":
+      addEmployee();
+      break;
 
-        case "remove Employee":
-          removeEmployee();
-          break;
+    case "Remove Employee":
+      removeEmployee();
+      break;
 
-        case "Update Employee Role":
-          updateEmployeeRole();
-          break;
+    case "Update Employee Role":
+      updateEmployeeRole();
+      break;
 
-        case "add Department":
-          addDepartment();
-          break;
+    case "Add Department":
+      addDepartment();
+      break;
 
-        case "add Role":
-          addRole();
-          break;
+    case "Add Role":
+      addRole();
+      break;
 
-        case "End session":
-          endSession();
-          break;
-      }
- 
+    case "End session":
+      endSession();
+      break;
+  }
 }
 //answer selections to move to relevant next stage ie roles or department etc
 async function viewDepartments() {
- const allDepartments = await databaseQueries.getAllDepartments(); 
- console.table(allDepartments)
- runSearch();
-    }
-  
-
-
-function viewEmployees() {
-  let query =
-    "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id";
-  query += "FROM EMPLOYEE";
-  query += "INNER JOIN role ON employee = department_name";
-
-  // need colums help not sure how to call query
-
-  connection.query(query, function (err, res) {
-    console.table("all Employees", res);
-    runSearch;
-  });
+  const allDepartments = await databaseQueries.getAllDepartments();
+  console.table(allDepartments);
+  runSearch();
+}
+async function viewRoles() {
+  const allRoles = await databaseQueries.getAllRoles();
+  console.table(allRoles);
+  runSearch();
+}
+async function viewEmployees() {
+  const allEmployees = await databaseQueries.getAllEmployees();
+  console.table(allEmployees);
+  runSearch();
+}
+async function viewEmployeesByManager() {
+  const allEmployees = await databaseQueries.getAllEmployeesByManager();
+  console.table(getAllEmployeesByManager);  
+  runSearch();
 }
 
-function viewEmployeesByManager() {
-  console.log("view employees by Manager");
-  let query =
-    "SELECT manager_id, manager name, employee.first_name, employee.last_name";
-  query += "FROM manager";
-  query += "INNER JOIN employee ON manager.id = employee.manager_id";
-  query += "ORDER BY manager.name";
+async function addDepartment() {
+  const newDepartment = await inquirer.prompt([
+    {
+      name: "name",
+      message: "What would you like to name the new department?",
+    },
+  ]);
 
-  connection.query(query, function (err, res) {
-    console.table("Employees By Manager", res);
-    runSearch;
-  });
+  await databaseQueries.addNewDepartment(newDepartment);
+  console.log(
+    "Successfully created new department. Select View Departments to review"
+  );
+  runSearch();
 }
+async function addEmployee() {
+  const NewEmployee = await inquirer.prompt([
+    {
+      name: "name",
+      message: "what role would you like your new employee to do?",
+      choices: [
+        "engineer",
+        "administrator",
+        "Closer"
+      ]
+    },
+  ]);
+//think I need code here to select employee role & put in correct department thik this might be a JOIN ??
+  await databaseQueries.addNewEmployee(NewEmployee);
+  console.log(
+    "Successfully created new employee. Select View Employees to review"
 
-
-
-
-
-
-
-
-
-
-
-
-
+  );
+  runSearch();
+}
 
 initialise();
